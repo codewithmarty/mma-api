96d893449966a3348fce180eafbd7c13177f83f0
Martin Nicola <lehitutoring@gmail.com>
2023-03-17 11:13:03 -0600

    feat: added momentjs to render last API hit

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,9 @@
 const Counter = require('../models/counter')
+const moment = require('moment')
 
 function index(req, res) {
     Counter.findOne().then(counter => {
-        res.render('index', { counter });
+        res.render('index', { counter, moment });
     })
 }
 
